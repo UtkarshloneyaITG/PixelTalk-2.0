a4c253afed050c3885aa4fc812946fbfd4b64572
ParthsharmaITG <parthsharma@itgeeks.com>
2025-11-04 20:11:05 +0530

    make changes in chat window design

--- frontend/src/Components/Chat-window.jsx
+++ frontend/src/Components/Chat-window.jsx
@@ -29,24 +29,35 @@ function ChatWindow() {
   }, []);
   return (
     <>
-      <div className="flex flex-col flex-1 px-10 pb-5 chat-window">
-        <div className="px-6 py-3 font-semibold Chat-Person--placeholder">
-          Alen Santos
-        </div>
-        <div className="p-6 overflow-auto space-y-4 Chat--Chat-placeholder ">
-          {messages.map((value, index) => {
-            return value.userID == 1 ? (
-              <SendBy
-                text={value.msg}
-                date={value.date}
-                time={value.time}
-                name={value.userID}
-                key={index}
-              />
-            ) : (
-              <SendTo text={value.msg} date={value.date} time={value.time} />
-            );
-          })}
+      <div className=" relative flex flex-col flex-1 px-10 pb-5 chat-window justify-end ">
+        <div className="h-full">
+          <div className="flex items-center justify-between pr-7">
+            <div className="profile flex items-center">
+              <div className="profile-image">
+              </div>
+              <div className="px-3 py-8  Chat-Person--placeholder text-2xl">
+                User
+              </div>
+            </div>
+              <div className="extra flex">
+                ●●●
+              </div>
+          </div>
+          <div className="p-6 overflow-auto space-y-4 Chat--Chat-placeholder h-screen fade-messages">
+            {messages.map((value, index) => {
+              return value.userID == 1 ? (
+                <SendBy
+                  text={value.msg}
+                  date={value.date}
+                  time={value.time}
+                  name={value.userID}
+                  key={index}
+                />
+              ) : (
+                <SendTo text={value.msg} date={value.date} time={value.time} />
+              );
+            })}
+          </div>
         </div>
         {/* {Chat Input} */}
         <ChatInput />
