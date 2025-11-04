9a383340c19e32fdddf912e40c6f4baf3e3b99de
ParthsharmaITG <parthsharma@itgeeks.com>
2025-11-04 16:07:56 +0530

    add roots css in file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,13 @@
-<<<<<<< HEAD
-import React from 'react'
-import ChatArea from './Components/ChatArea';
-import SignUp from './Pages/SignUp';
 
-const App = () => {
-  return (
-    <SignUp />
-  )
-}
-=======
-import React from "react";
-// import ChatArea from "./Components/ChatArea";
-// import MessageBox from "./Components/MessageBox";
+import SignUp from './Pages/SignUp';
 import ChatApp from "./Chat";
 
 const App = () => {
   return (
     <>
-      <ChatApp />
+      <SignUp />
+      {/* <ChatApp /> */}
     </>
   );
 };
->>>>>>> 40777184eae3e2ee9bf2f77034b0d6dccb8aff86
-
 export default App;
