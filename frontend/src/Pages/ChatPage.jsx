86e52f5cc9b66da9a21d8a69925ecc593021477d
ParthsharmaITG <parthsharma@itgeeks.com>
2025-11-04 19:07:22 +0530

    make changes into chat window

--- frontend/src/Pages/ChatPage.jsx
+++ frontend/src/Pages/ChatPage.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import MessageBox from '../Components/MessageBox'
+// import React from 'react'
+// // import MessageBox from '../Components/MessageBox'
 
-const ChatPage = () => {
-  return (
-    <div className='chat-wrapper'>
-      <div className='chat-area'>
-        <MessageBox />
-      </div>
-    </div>
-  )
-}
+// const ChatPage = () => {
+//   return (
+//     <div className='chat-wrapper'>
+//       <div className='chat-area'>
+//         <MessageBox />
+//       </div>
+//     </div>
+//   )
+// }
 
-export default ChatPage
+// export default ChatPage
