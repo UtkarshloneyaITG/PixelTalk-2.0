7473895f39cf66c4fb7272dade39723b82262515
ParthsharmaITG <parthsharma@itgeeks.com>
2025-11-05 12:10:29 +0530

    change msg aniamtion

--- frontend/src/Components/ChatSendBy.jsx
+++ frontend/src/Components/ChatSendBy.jsx
@@ -1,4 +1,17 @@
+import { useGSAP } from "@gsap/react";
+import gsap from 'gsap';
+
 function SendBy({ text, date, time, name }) {
+
+  useGSAP(() => {
+    gsap.from(".ChatSendBy-- ", {
+      y: 20,
+      duration: .3,
+      ease: 'elastic.out',
+    })
+  })
+
+
   return (
     <>
       <div className="  ChatSendBy-- ">
